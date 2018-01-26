423097954b2c1301ad36ffbf2fe68b4560ab226f
Adam Zhu <azhu@cbancnetwork.com>
2018-01-26 14:53:18 -0600

    add an example piece of js code

---
+++ index.js
@@ -0,0 +1,41 @@
+const data = [{
+  title: 'a',
+  description: 'description a',
+  date: 2
+}, {
+  title: 'b',
+  description: 'description b',
+  date: 3
+}, {
+  title: 'c',
+  description: 'description c',
+  date: 1
+}];
+
+const create_sort = prop => order => (a, b) => {
+  const a_val = a[prop];
+  const b_val = b[prop];
+
+  if (a_val > b_val) {
+    return order === 'asc'
+    ? 1
+    : -1;
+  }
+  else if (a_val < b_val) {
+    return order === 'asc'
+    ? -1
+    : 1;
+  }
+
+  return 0;
+};
+
+const by_title_desc = create_sort('title')('desc');
+const by_date_asc = create_sort('date')('asc');
+
+const sorted_by_title = data.slice().sort(by_title_desc);
+const sorted_by_date = data.slice().sort(by_date_asc);
+
+console.log('unsorted data', data);
+console.log('sorted by title descending', sorted_by_title);
+console.log('sorted by date ascending', sorted_by_date);
