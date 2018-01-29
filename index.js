ccb79da37cc816f6be45b5f2657b7fefa10fddc5
Adam Zhu <azhu@cbancnetwork.com>
2018-01-29 10:00:24 -0600

    add pick_props util

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const data = [{
-  title: 'a',
-  description: 'description a',
-  date: 2
-}, {
-  title: 'b',
-  description: 'description b',
-  date: 3
-}, {
-  title: 'c',
-  description: 'description c',
-  date: 1
-}];
-
 // if order is not explicity 'desc' then it will be asc
 const create_sort = prop => order => (a, b) => {
   const a_val = a[prop];
@@ -31,12 +17,50 @@ const create_sort = prop => order => (a, b) => {
   return 0;
 };
 
-const by_title_desc = create_sort('title')('desc');
-const by_date_asc = create_sort('date')('asc');
+const scrub_props = props => Object.keys(props).reduce((clean, k) =>
+  props[k] !== undefined && props[k] !== null
+  ? Object.assign(clean, { [k]: props[k] })
+  : clean
+, {});
 
-const sorted_by_title = data.slice().sort(by_title_desc);
-const sorted_by_date = data.slice().sort(by_date_asc);
+const pick_props = (keys, source_obj) => scrub_props(keys.map(k => source_obj[k]));
 
-console.log('unsorted data', data);
-console.log('sorted by title descending', sorted_by_title);
-console.log('sorted by date ascending', sorted_by_date);
+module.exports = {
+  create_sort,
+  pick_props
+};
+
+
+// demo usage
+// const data = [{
+//   title: 'a',
+//   description: 'description a',
+//   date: 2
+// }, {
+//   title: 'b',
+//   description: 'description b',
+//   date: 3
+// }, {
+//   title: 'c',
+//   description: 'description c',
+//   date: 1
+// }];
+// const by_title_desc = create_sort('title')('desc');
+// const by_date_asc = create_sort('date')('asc');
+// const sorted_by_title = data.slice().sort(by_title_desc);
+// const sorted_by_date = data.slice().sort(by_date_asc);
+
+// console.log('unsorted data', data);
+// console.log('sorted by title descending', sorted_by_title);
+// console.log('sorted by date ascending', sorted_by_date);
+
+
+// const from_this_obj = {
+//   a: '1',
+//   b: '2',
+//   c: '3',
+//   d: '4'
+// };
+// const these_props = ['a', 'b', 'c', 'e'];
+
+// console.log(pick_props(these_props, from_this_obj));
